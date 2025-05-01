24dd2a8493f4af691b0a48031b644a8602ab7832
Sir Wabbit <559086+sir-wabbit@users.noreply.github.com>
2025-04-30 22:03:30 -0400

    Bump version to 0.1.0
    Updated build.gradle.kts by changing the project version and the Maven publication version from 0.0.1 to 0.1.0 to reflect the new release version.
    
    Semver Impact: MINOR

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ repositories {
 }
 
 group   = "one.wabbit"
-version = "0.0.1"
+version = "0.1.0"
 
 plugins {
     kotlin("jvm") version "2.1.20"
@@ -23,7 +23,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "one.wabbit"
             artifactId = "kotlin-web-springernature"
-            version = "0.0.1"
+            version = "0.1.0"
             from(components["java"])
         }
     }
